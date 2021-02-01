8749b976607404b58b75d8a908065cab98133758
Wilhelm Osada <235940@edu.p.lodz.pl>
2021-02-01 12:55:25 +0100

    01 Zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+int Policz(int a, int b)
+{
+    return a+b;
+}
 using namespace std;
 int main() {
     cout << "Hello, World!";
+    cout << "W pierwszym branchu; 2+3="<< Policz(2,3)<<endl;
     return 0;
 }

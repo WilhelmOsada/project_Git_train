45da167ebb6ac189a94d5d26b600ef4038fcc97d
Wilhelm Osada <235940@edu.p.lodz.pl>
2021-02-01 13:04:53 +0100

    02 Zmiana mnożenia metody Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
+int Policz(int a, int b)
+{
+    return a*b;
+}
 int main() {
     cout << "Hello, World!";
+    cout << "W pierwszym branchu 2*3=" << Policz(2,3)<<endl;
     return 0;
 }
